cf9e031e9e25cde4924c514109d4e1d85d707b21
Manshilin Oleg <manshilin.o@gmail.com>
2023-04-04 23:53:21 +0300

    Advent of code 2022 in haskell First commit  Changes to be committed: 	new file:   .gitignore 	new file:   adventofcode2022.cabal 	new file:   app/Main.hs

---
+++ app/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Data.List (sort)
+import Data.List.Split (splitOn)
+
+parseString :: String -> [[Int]]
+parseString = map (map read . lines) . splitOn "\n\n"
+
+computeAnsw :: [[Int]] -> Int -> [Int]
+computeAnsw takeList quant = take quant . reverse . sort . map sum $ takeList
+
+main :: IO ()
+main = do
+  print "yay"
+  file_str <- readFile "task_1.txt"
+  print $ head . computeAnsw (parseString file_str) $ 1
+  print $ sum . computeAnsw (parseString file_str) $ 3

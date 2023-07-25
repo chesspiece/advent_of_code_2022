743bf4e01df18564aa0ab7aa3583888a4e098a74
Manshilin Oleg <manshilin.o@gmail.com>
2023-07-25 19:13:03 +0300

    Solved task 5 part 1  On branch main  Changes to be committed: 	modified:   app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,23 +2,20 @@
 
 module Main (splitList, main) where
 
-import Control.Applicative
+import Control.Lens
 import Data.List.Split (splitOn)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromJust, fromMaybe)
 import Data.Stack (Stack, stackNew, stackPeek, stackPop, stackPush)
 
 splitList :: forall a. (Eq a) => a -> [a] -> Maybe ([a], [a])
 splitList = splitListHelper []
   where
     splitListHelper :: [a] -> a -> [a] -> Maybe ([a], [a])
-    splitListHelper accum bb (h : rest) =
-      if h == bb
+    splitListHelper accum splitSymb (h : rest) =
+      if h == splitSymb
         then Just (accum, rest)
-        else splitListHelper (accum ++ [h]) bb rest
+        else splitListHelper (accum ++ [h]) splitSymb rest
     splitListHelper _ _ [] = Nothing
-
-findNumCrates :: String -> Int
-findNumCrates = flip div 3 . length
 
 parseInput :: String -> ([String], [[Int]])
 parseInput input_text = (init $ map parseInputState states, map parseInputActions actions)
@@ -28,7 +25,7 @@ parseInput input_text = (init $ map parseInputState states, map parseInputAction
       Nothing -> error "Was unable to parse actions description"
 
 parseInputActions :: String -> [Int]
-parseInputActions action_desc = map (read . filter (\x -> x >= '1' && x <= '9')) $ concatMap (splitOn "to") (splitOn "from" action_desc)
+parseInputActions action_desc = map (read . filter (\x -> x >= '0' && x <= '9')) $ concatMap (splitOn "to") (splitOn "from" action_desc)
 
 parseInputState :: String -> String
 parseInputState action_desc = takeNthElems 3 preproc_list
@@ -39,11 +36,8 @@ takeNthElems :: Int -> [a] -> [a]
 takeNthElems n (x : xs) = x : takeNthElems n (drop n xs)
 takeNthElems _ [] = []
 
-createNStack :: Int -> [Stack Char]
-createNStack n = [stackNew | _ <- [1 .. n]]
-
 createState :: [String] -> [Stack Char]
-createState state = foldl (addStateHelper []) (createNStack (length state)) state
+createState state = foldl (addStateHelper []) [stackNew | _ <- [1 .. (length (state !! 1))]] true_state
   where
     addStateHelper :: [Stack Char] -> [Stack Char] -> String -> [Stack Char]
     addStateHelper accum (st : stt) (ch : rest) = addStateHelper (accum ++ [new_stack]) stt rest
@@ -53,17 +47,28 @@ createState state = foldl (addStateHelper []) (createNStack (length state)) stat
           _ -> stackPush st ch
     addStateHelper accum [] [] = accum
     addStateHelper _ _ _ = error "Everything is wrong"
+    true_state = reverse state
 
 doAction :: [Stack Char] -> [Int] -> [Stack Char]
-doAction stcks [a, b, c] = stcks
-    where (new_state, chr) =
-        case stackPop (stcks !! a) of
-            Just (new_state, chr) -> (new_state, chr)
-            Nothing -> (stcks !! a, ' ')
+doAction stcks [0, _, _] = stcks
+doAction stcks [a, b, c] = doAction ((stcks & ix bb .~ new_state) & ix cc .~ stackPush (stcks !! cc) chr) [a - 1, b, c]
+  where
+    (new_state, chr) = fromJust (stackPop (stcks !! bb))
+    bb = b - 1
+    cc = c - 1
 doAction _ _ = error ":(((("
+
+doAction2 :: [Stack Char] -> [Int] -> [Stack Char]
+doAction2 stcks [0, _, _] = stcks
+doAction2 stcks [a, b, c] = doAction ((stcks & ix bb .~ new_state) & ix cc .~ stackPush (stcks !! cc) chr) [a - 1, b, c]
+  where
+    (new_state, chr) = fromJust (stackPop (stcks !! bb))
+    bb = b - 1
+    cc = c - 1
+doAction2 _ _ = error ":(((("
 
 main :: IO ()
 main = do
   file_str <- readFile "./task_5.txt"
-  (state, actions) <- pure $ parseInput file_str
-  print "Yay"
+  (state_inp, actions) <- pure $ parseInput file_str
+  print $ map (fromJust . stackPeek) (foldl doAction (createState state_inp) actions)

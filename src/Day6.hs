311cef94974dcb624b2f36caf29cb2a39e1ef248
Manshilin Oleg <manshilin.o@gmail.com>
2023-08-11 20:24:43 +0300

    Solved second part of day 6  Changes to be committed: 	modified:   src/Day6.hs

--- src/Day6.hs
+++ src/Day6.hs
@@ -3,18 +3,19 @@
 {-# HLINT ignore "Use if" #-}
 module Day6 (day6) where
 
-parseInputString :: String -> Int
-parseInputString = parseInputStringHelper 0 0 ""
+parseInputString :: Int -> String -> Int
+parseInputString marker_condition = parseInputStringHelper marker_condition (-marker_condition) 0 ""
   where
-    parseInputStringHelper :: Int -> Int -> String -> String -> Int
-    parseInputStringHelper 4 acc _ _ = acc
-    parseInputStringHelper chk acc check_list (c : str) = case c `elem` check_list of
-      True -> parseInputStringHelper (length new_check_list) (acc + 1) new_check_list str
-      False -> parseInputStringHelper (chk + 1) (acc + 1) (check_list ++ [c]) str
+    parseInputStringHelper :: Int -> Int -> Int -> String -> String -> Int
+    parseInputStringHelper _ 0 acc _ _ = acc
+    parseInputStringHelper marker_condition chk acc check_list (c : str) = case c `elem` check_list of
+      True -> parseInputStringHelper marker_condition (length new_check_list - marker_condition) (acc + 1) new_check_list str
+      False -> parseInputStringHelper marker_condition (chk + 1) (acc + 1) (check_list ++ [c]) str
       where
         new_check_list = tail . dropWhile (/= c) $ check_list ++ [c]
 
 day6 :: IO ()
 day6 = do
   input_stream <- readFile "./task_6.txt"
-  print $ parseInputString input_stream
+  print $ parseInputString 4 input_stream
+  print $ parseInputString 14 input_stream

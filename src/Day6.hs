515c09347c215f0e694eea505f8a621248537a0a
Manshilin Oleg <manshilin.o@gmail.com>
2023-08-20 07:39:06 +0300

    Very small refactoring  Changes to be committed: 	modified:   src/Day6.hs

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
-{-# HLINT ignore "Use if" #-}
 module Day6 (day6) where
 
 parseInputString :: Int -> String -> Int
@@ -8,9 +5,11 @@ parseInputString marker_condition = parseInputStringHelper marker_condition (-ma
   where
     parseInputStringHelper :: Int -> Int -> Int -> String -> String -> Int
     parseInputStringHelper _ 0 acc _ _ = acc
-    parseInputStringHelper marker_condition chk acc check_list (c : str) = case c `elem` check_list of
-      True -> parseInputStringHelper marker_condition (length new_check_list - marker_condition) (acc + 1) new_check_list str
-      False -> parseInputStringHelper marker_condition (chk + 1) (acc + 1) (check_list ++ [c]) str
+    parseInputStringHelper marker_condition chk acc check_list (c : str) = if c `elem` check_list
+      then
+        parseInputStringHelper marker_condition (length new_check_list - marker_condition) (acc + 1) new_check_list str
+      else
+        parseInputStringHelper marker_condition (chk + 1) (acc + 1) (check_list ++ [c]) str
       where
         new_check_list = tail . dropWhile (/= c) $ check_list ++ [c]
     parseInputStringHelper _ _ _ _ _ = error "Wrong input for this task"

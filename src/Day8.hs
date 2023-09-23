a944c5ba5edbe7b7a32667f118e06ece6298d6e2
Manshilin Oleg <manshilin.o@gmail.com>
2023-09-23 20:56:52 +0300

    Day 8 part 1 is solved. I feel terrified by my solution  Changes to be committed: 	modified:   src/Day8.hs

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
@@ -15,31 +16,125 @@ data TaskState = TaskState {_boardState :: [[Int]], _compState :: [[Int]], _maxC
 makeLenses ''TaskState
 
 firstRun :: Int -> Int -> State TaskState [[Int]]
-firstRun 0 j = do firstRun 1 0
 firstRun i 0 = do firstRun i 1
 firstRun i j = do
   currSt <- get
-  if i /= _maxRows currSt || j /= _maxColumns currSt then do
-    let choice1 = max ((_boardState currSt !! i) !! (j - 1)) ((_compState currSt !! i) !! (j - 1)) 
-    let choice2 = max ((_boardState currSt !! (i - 1)) !! j) ((_compState currSt !! (i - 1)) !! j)
-    let curr_sol = _compState currSt & ix i . ix j .~ min choice1 choice2
-    put $
-      TaskState
-        { _boardState = _boardState currSt,
-          _compState = curr_sol,
-          _maxColumns = _maxColumns currSt,
-          _maxRows = _maxRows currSt
-        }
-    if j == 4
-      then firstRun (i + 1) 0
-      else firstRun i (j + 1)
-  else do _compState <$> get
+  if i /= _maxRows currSt || j /= _maxColumns currSt
+    then do
+      let choice = max ((_boardState currSt !! i) !! (j - 1)) ((_compState currSt !! i) !! (j - 1))
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $
+        TaskState
+          { _boardState = _boardState currSt,
+            _compState = curr_sol,
+            _maxColumns = _maxColumns currSt,
+            _maxRows = _maxRows currSt
+          }
+      if j == _maxColumns currSt
+        then firstRun (i + 1) 0
+        else firstRun i (j + 1)
+    else do _compState <$> get
+
+secondRun :: Int -> Int -> State TaskState [[Int]]
+secondRun 0 j = do secondRun 1 0
+secondRun i j = do
+  currSt <- get
+  if i /= _maxRows currSt || j /= _maxColumns currSt
+    then do
+      let choice = max ((_boardState currSt !! (i - 1)) !! j) ((_compState currSt !! (i - 1)) !! j)
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $
+        TaskState
+          { _boardState = _boardState currSt,
+            _compState = curr_sol,
+            _maxColumns = _maxColumns currSt,
+            _maxRows = _maxRows currSt
+          }
+      if j == _maxColumns currSt
+        then secondRun (i + 1) 0
+        else secondRun i (j + 1)
+    else do _compState <$> get
+
+thirdRun :: Int -> Int -> State TaskState [[Int]]
+thirdRun 0 0 = _compState <$> get
+thirdRun i j = do
+  currSt <- get
+  let checkColumns = _maxColumns currSt
+  let checkRows = _maxRows currSt
+  if i == checkRows
+    then do
+      let curr_sol = _compState currSt & ix i . ix j .~ (-1)
+      put $
+        TaskState
+          { _boardState = _boardState currSt,
+            _compState = curr_sol,
+            _maxColumns = _maxColumns currSt,
+            _maxRows = _maxRows currSt
+          }
+      if j == 0
+        then thirdRun (i - 1) (_maxColumns currSt)
+        else thirdRun i (j - 1)
+    else do
+      let choice = max ((_boardState currSt !! (i + 1)) !! j) ((_compState currSt !! (i + 1)) !! j)
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $
+        TaskState
+          { _boardState = _boardState currSt,
+            _compState = curr_sol,
+            _maxColumns = _maxColumns currSt,
+            _maxRows = _maxRows currSt
+          }
+      if j == 0
+        then thirdRun (i - 1) (_maxColumns currSt)
+        else thirdRun i (j - 1)
+
+forthRun :: Int -> Int -> State TaskState [[Int]]
+forthRun 0 0 = _compState <$> get
+forthRun i j = do
+  currSt <- get
+  let checkColumns = _maxColumns currSt
+  let checkRows = _maxRows currSt
+  if j == checkColumns
+    then do
+      let curr_sol = _compState currSt & ix i . ix j .~ (-1)
+      put $
+        TaskState
+          { _boardState = _boardState currSt,
+            _compState = curr_sol,
+            _maxColumns = _maxColumns currSt,
+            _maxRows = _maxRows currSt
+          }
+      forthRun i (j - 1)
+    else do
+      let choice = max ((_boardState currSt !! i) !! (j + 1)) ((_compState currSt !! i) !! (j + 1))
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $
+        TaskState
+          { _boardState = _boardState currSt,
+            _compState = curr_sol,
+            _maxColumns = _maxColumns currSt,
+            _maxRows = _maxRows currSt
+          }
+      if j == 0
+        then forthRun (i - 1) (_maxColumns currSt)
+        else forthRun i (j - 1)
 
 day8 :: IO ()
 day8 = do
   input_stream <- readFile "./task_8.txt" >>= return . map (map digitToInt) . lines
   let s1 = length input_stream
   let s2 = length $ head input_stream
-  let initial_sol = replicate s1 $ replicate s2 0
-  let task = TaskState {_boardState = input_stream , _compState = initial_sol , _maxColumns = s1 - 1, _maxRows = s2 - 1}
-  print $ evalState (firstRun 0 0) task
+  let initial_sol = replicate s1 $ replicate s2 (-1)
+  let task = TaskState {_boardState = input_stream, _compState = initial_sol, _maxColumns = s1 - 1, _maxRows = s2 - 1}
+  let firstHalf = evalState (firstRun 0 0) task
+  let secondHalf = evalState (secondRun 0 0) task
+  let thirdHalf = evalState (thirdRun (s1 -1) (s2 - 1)) task
+  let fourthHalf = evalState (forthRun (s1 - 1) (s2 - 1)) task
+  let firstResHalf = [[ min x1 y1 | (x1, y1) <- zip x y] | (x, y) <- zip firstHalf secondHalf]
+  let secondResHalf = [[ min x1 y1 | (x1, y1) <- zip x y] | (x, y) <- zip firstResHalf thirdHalf]
+  let thirdResHalf = [[ min x1 y1 | (x1, y1) <- zip x y] | (x, y) <- zip secondResHalf fourthHalf]
+  let res = [[ if x1 < y1 then 1 else 0 | (x1, y1) <- zip x y] | (x, y) <- zip thirdResHalf input_stream]
+  print thirdResHalf
+  print input_stream
+  print res
+  print $ sum $ map sum res

eab4f8a3fb6f8aa5c1d5e936d11f93e1aa45ec17
Manshilin Oleg <manshilin.o@gmail.com>
2023-10-02 00:51:07 +0300

    Solved day 8 part two  Changes to be committed: 	modified:   src/Day8.hs

--- src/Day8.hs
+++ src/Day8.hs
@@ -9,8 +9,10 @@ module Day8 (day8) where
 import Control.Lens
 import Control.Monad.State (MonadState (get, put), State, evalState, gets, runState)
 import Data.Char (digitToInt)
+import Debug.Trace (trace, traceM)
+import qualified Data.Vector as V
 
-data TaskState = TaskState {_boardState :: [[Int]], _compState :: [[Int]], _maxColumns :: Int, _maxRows :: Int}
+data TaskState = TaskState {_boardState :: [[Int]], _compState :: [[Int]], _maxColumns :: Int, _maxRows :: Int, _helperVec :: V.Vector Int}
   deriving (Show)
 
 makeLenses ''TaskState
@@ -83,13 +85,115 @@ forthRun i j = do
         then forthRun (i - 1) (_maxColumns currSt)
         else forthRun i (j - 1)
 
+fillHelperVector :: V.Vector Int -> Int -> Int -> V.Vector Int
+fillHelperVector vec val pos = vec V.// [(i, pos) | i <- [0..val]]
+
+firstRun2 :: Int -> Int -> State TaskState [[Int]]
+firstRun2 i j = do
+  currSt <- get
+  if i /= _maxRows currSt || j /= _maxColumns currSt
+    then do
+      let tree_height = (_boardState currSt !! i) !! j
+      let choice = j - (_helperVec currSt V.! tree_height)
+      let new_vec = fillHelperVector (_helperVec currSt) tree_height j
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $ (currSt & compState .~ curr_sol) & helperVec .~ new_vec
+      if j == _maxColumns currSt
+        then do
+          put $ currSt & helperVec .~ V.replicate 10 0
+          firstRun2 (i + 1) 0
+        else
+          firstRun2 i (j + 1)
+    else do
+      let tree_height = (_boardState currSt !! i) !! j
+      let choice = j - (_helperVec currSt V.! tree_height)
+      let new_vec = fillHelperVector (_helperVec currSt) tree_height j
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $ (currSt & compState .~ curr_sol) & helperVec .~ new_vec
+      _compState <$> get
+
+secondRun2 :: Int -> Int -> State TaskState [[Int]]
+secondRun2 i j = do
+  currSt <- get
+  if i /= _maxRows currSt || j /= _maxColumns currSt
+    then do
+      let tree_height = (_boardState currSt !! i) !! j
+      let choice = i - (_helperVec currSt V.! tree_height)
+      let new_vec = fillHelperVector (_helperVec currSt) tree_height i
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $ (currSt & compState .~ curr_sol) & helperVec .~ new_vec
+      if i == _maxRows currSt
+        then do
+          put $ currSt & helperVec .~ V.replicate 10 0
+          secondRun2 0 (j + 1)
+        else secondRun2 (i + 1) j
+    else do
+      let tree_height = (_boardState currSt !! i) !! j
+      let choice = i - (_helperVec currSt V.! tree_height)
+      let new_vec = fillHelperVector (_helperVec currSt) tree_height i
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $ (currSt & compState .~ curr_sol) & helperVec .~ new_vec
+      _compState <$> get
+
+thirdRun2 :: Int -> Int -> State TaskState [[Int]]
+thirdRun2 i j = do
+  currSt <- get
+  if i /= 0 || j /= 0
+    then do
+      let tree_height = (_boardState currSt !! i) !! j
+      let choice = (_helperVec currSt V.! tree_height) - j
+      let new_vec = fillHelperVector (_helperVec currSt) tree_height j
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $ (currSt & compState .~ curr_sol) & helperVec .~ new_vec
+      if j == 0
+        then do
+          put $ currSt & helperVec .~ V.replicate 10 (_maxColumns currSt)
+          thirdRun2 (i - 1) (_maxColumns currSt)
+        else
+          thirdRun2 i (j - 1)
+    else do
+      let tree_height = (_boardState currSt !! i) !! j
+      let choice = (_helperVec currSt V.! tree_height) - j
+      let new_vec = fillHelperVector (_helperVec currSt) tree_height j
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $ (currSt & compState .~ curr_sol) & helperVec .~ new_vec
+      _compState <$> get
+
+forthRun2 :: Int -> Int -> State TaskState [[Int]]
+forthRun2 0 0 = _compState <$> get
+forthRun2 i j = do
+  currSt <- get
+  if i /= 0 || j /= 0
+    then do
+      let tree_height = (_boardState currSt !! i) !! j
+      let choice = (_helperVec currSt V.! tree_height) - i
+      let new_vec = fillHelperVector (_helperVec currSt) tree_height i
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $ (currSt & compState .~ curr_sol) & helperVec .~ new_vec
+      if i == 0
+        then do
+          put $ currSt & helperVec .~ V.replicate 10 (_maxRows currSt)
+          forthRun2 (_maxRows currSt) (j - 1)
+        else
+          forthRun2 (i - 1) j
+    else do
+      let tree_height = (_boardState currSt !! i) !! j
+      let choice = (_helperVec currSt V.! tree_height) - i
+      let new_vec = fillHelperVector (_helperVec currSt) tree_height i
+      let curr_sol = _compState currSt & ix i . ix j .~ choice
+      put $ (currSt & compState .~ curr_sol) & helperVec .~ new_vec
+      _compState <$> get
+
 day8 :: IO ()
 day8 = do
   input_stream <- readFile "./task_8.txt" >>= return . map (map digitToInt) . lines
   let s1 = length input_stream
   let s2 = length $ head input_stream
   let initial_sol = replicate s1 $ replicate s2 (-1)
-  let task = TaskState {_boardState = input_stream, _compState = initial_sol, _maxColumns = s1 - 1, _maxRows = s2 - 1}
+  let initial_vec = V.replicate 10 0
+  let initial_vec_rev_rows = V.replicate 10 (s1 - 1)
+  let initial_vec_rev_columns = V.replicate 10 (s2 - 1)
+  let task = TaskState {_boardState = input_stream, _compState = initial_sol, _maxColumns = s1 - 1, _maxRows = s2 - 1, _helperVec = initial_vec}
   let firstPart = evalState (firstRun 0 0) task
   let secondPart = evalState (secondRun 0 0) task
   let thirdPart = evalState (thirdRun (s1 - 1) (s2 - 1)) task
@@ -99,3 +203,16 @@ day8 = do
   let thirdResPart = [[min x1 y1 | (x1, y1) <- zip x y] | (x, y) <- zip secondResPart fourthPart]
   let res = [[if x1 < y1 then 1 else 0 | (x1, y1) <- zip x y] | (x, y) <- zip thirdResPart input_stream]
   print $ sum $ map sum res
+
+  let initial_sol2 = replicate s1 $ replicate s2 0
+  let task2 = TaskState {_boardState = input_stream, _compState = initial_sol2, _maxColumns = s1 - 1, _maxRows = s2 - 1, _helperVec = initial_vec}
+  let task2_rev_columns = TaskState {_boardState = input_stream, _compState = initial_sol2, _maxColumns = s1 - 1, _maxRows = s2 - 1, _helperVec = initial_vec_rev_columns}
+  let task2_rev_rows = TaskState {_boardState = input_stream, _compState = initial_sol2, _maxColumns = s1 - 1, _maxRows = s2 - 1, _helperVec = initial_vec_rev_rows}
+  let firstHalf2 = evalState (firstRun2 0 0) task2
+  let secondHalf2 = evalState (secondRun2 0 0) task2
+  let thirdHalf2 = evalState (thirdRun2 (s1 - 1) (s2 - 1)) task2_rev_columns
+  let fourthHalf2 = evalState (forthRun2 (s1 - 1) (s2 - 1)) task2_rev_rows
+  let firstResHalf2 = [[x1 * y1 | (x1, y1) <- zip x y] | (x, y) <- zip firstHalf2 secondHalf2]
+  let secondResHalf2 = [[x1 * y1 | (x1, y1) <- zip x y] | (x, y) <- zip firstResHalf2 thirdHalf2]
+  let res = [[x1 * y1 | (x1, y1) <- zip x y] | (x, y) <- zip secondResHalf2 fourthHalf2]
+  print $ maximum $ map maximum res

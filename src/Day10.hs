dbf74b1e5bfcda015cabe8f9d28a9ff1436a5350
Manshilin Oleg <manshilin.o@gmail.com>
2025-01-06 13:35:07 +0000

    Formatting and renaming TaskState10 to TaskState

--- src/Day10.hs
+++ src/Day10.hs
@@ -28,29 +28,29 @@ import Control.Monad.State (
  )
 import Data.Maybe (fromJust)
 import Data.Void (Void)
+import Debug.Trace (trace, traceIO, traceM)
 import Text.Megaparsec.Char (newline, string)
 import Text.Megaparsec.Char.Lexer (decimal, signed)
-import Debug.Trace (traceM, trace, traceIO)
 
 data Instruction = Noop | Addx Int
     deriving (Show, Eq, Ord)
 newtype CycleVal = CycleVal (Int, Int)
 type Parser = Parsec Void String
 
-data TaskState10 = TaskState10
+data TaskState = TaskState10
     { _timerCount :: Int
     , _regCount :: Int
     }
     deriving (Show)
 
-makeLenses ''TaskState10
+makeLenses ''TaskState
 
 oneLineSequence :: Parser Instruction
 oneLineSequence =
     (Noop <$ (string "noop" <* many newline)) <|> Addx
         <$> ((string "addx ") *> (signed (return ()) decimal) <* many newline)
 
-oneInstructionProcessing :: Instruction -> State TaskState10 Int
+oneInstructionProcessing :: Instruction -> State TaskState Int
 oneInstructionProcessing Noop = do
     curr_state <- get
     let new_timer_count = _timerCount curr_state + 1
@@ -73,32 +73,25 @@ checkIfVisible pos spritePos
     | pos == spritePos || pos == (spritePos - 1) || pos == (spritePos + 1) = True
     | otherwise = False
 
-
 jumpLine :: Int -> IO ()
 jumpLine n
-    | mod n 40 == 0 = putStr"\n"
+    | mod n 40 == 0 = putStr "\n"
     | otherwise = return ()
 
-
 printSprites :: Bool -> Int -> Int -> Int -> IO ()
-printSprites True 1 tmr spritePos = do
-    jumpLine tmr
-    putStr "#"
-printSprites False 1 tmr spritePos = do
-    jumpLine tmr
-    putStr "."
+printSprites True 1 tmr spritePos = jumpLine tmr >> putStr "#"
+printSprites False 1 tmr spritePos = jumpLine tmr >> putStr "."
 printSprites True n tmr spritePos = do
     jumpLine tmr
     putStr "#"
-    --traceIO (show n)
+    -- traceIO (show n)
     printSprites (checkIfVisible (mod tmr 40) spritePos) (n - 1) (tmr + 1) spritePos
 printSprites False n tmr spritePos = do
     jumpLine tmr
     putStr "."
     printSprites (checkIfVisible (mod tmr 40) spritePos) (n - 1) (tmr + 1) spritePos
 
-
-instructionsProcessing :: [Instruction] -> Int -> Int -> StateT TaskState10 IO Int
+instructionsProcessing :: [Instruction] -> Int -> Int -> StateT TaskState IO Int
 instructionsProcessing [] _ accum = return $ accum
 instructionsProcessing (instruction : instrscs) next_timer accum = do
     prev_state <- get
@@ -107,7 +100,12 @@ instructionsProcessing (instruction : instrscs) next_timer accum = do
     let prev_timer = _timerCount prev_state
     let spritePos = _regCount prev_state
     let current_timer = _timerCount curr_state
-    liftIO $ printSprites (checkIfVisible (mod prev_timer 40) spritePos) (current_timer - prev_timer) prev_timer spritePos
+    liftIO $
+        printSprites
+            (checkIfVisible (mod prev_timer 40) spritePos)
+            (current_timer - prev_timer)
+            prev_timer
+            spritePos
     if (current_timer + 1) == next_timer
         then
             instructionsProcessing instrscs (next_timer + 40) (accum + next_timer * (_regCount curr_state))

e4f7f0bb068ba6395f92dacd4d61c8ea95c696da
Manshilin Oleg <manshilin.o@gmail.com>
2025-02-16 18:59:24 +0000

    Writing parser for day 11. Added hastables for fast, imperative hastables

--- src/Day11.hs
+++ src/Day11.hs
@@ -6,14 +6,24 @@
 module Day11 (day11) where
 
 import Control.Applicative (many, (*>), (<*))
+import Control.Monad.Trans (lift)
 import Text.Megaparsec (
     Parsec,
+    ParsecT,
     eof,
+    option,
+    optional,
     parseMaybe,
+    runParser,
+    runParserT,
+    try,
     (<|>),
  )
 
+import Control.Monad.ST (ST)
+
 import Control.Lens (Identity (runIdentity), makeLenses, (&), (.~))
+import Control.Monad (liftM)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.State (
     MonadState (get, put),
@@ -26,16 +36,52 @@ import Control.Monad.State (
     runStateT,
     state,
  )
-import Data.Maybe (fromJust)
+
+-- (decimal, signed)
+
+import Control.Arrow (Arrow (second))
+import qualified Data.HashTable.IO as H
+import Data.Maybe (fromJust, fromMaybe)
 import Data.Void (Void)
 import Text.Megaparsec.Char (newline, string)
-import Text.Megaparsec.Char.Lexer (decimal, signed)
+import Text.Megaparsec.Char.Lexer as L
 
-type Parser = Parsec Void String
+type HashTable k v = H.BasicHashTable k v
+type Parser = ParsecT Void String (StateT (HashTable Integer [Integer]) IO)
+type ParserInner = Parsec Void String
 
-monkeyParse :: Parser Int
-monkeyParse = string "Monkey " *> decimal <* string ":" <* newline
+data TaskState = TaskState
+    { _timerCount :: Integer
+    , _regCount :: Integer
+    }
+    deriving (Show)
+
+monkeyParse :: Parser [Integer]
+monkeyParse =
+    do
+        ht <- get
+        string "Monkey "
+        monkey_index <- L.decimal
+        string ":"
+        newline
+        itms <- itemsParse
+        lift . lift $ H.insert ht monkey_index itms
+        lift . lift $ fromJust <$> H.lookup ht monkey_index
+
+itemsParse :: Parser [Integer]
+itemsParse =
+    do
+        string "Starting items: "
+        many (L.decimal <* (string ", " <|> many newline))
+
+str :: String
+str = "Monkey 101:\nStarting items: 1, 2, 3, 4\n"
 
 day11 :: IO ()
 day11 = do
+    new_hashtable <- H.new
+    tst <- runStateT (runParserT monkeyParse "" str) new_hashtable --
+    case tst of
+        (Left err, s) -> print "Error: parsing of input has failed"
+        (Right xs, s) -> print xs
     print "yay"

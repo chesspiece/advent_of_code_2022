d35fa8d9516384e65131625ce6dde9741a473138
Manshilin Oleg <manshilin.o@gmail.com>
2025-05-17 08:45:06 +0000

    Created function tofind index of start and end positions in maze

--- src/Day12.hs
+++ src/Day12.hs
@@ -6,11 +6,53 @@
 
 {-# HLINT ignore "Redundant if" #-}
 
--- I use ParsecT monad transformer here in order to ahve an example of using Parsec subparser
--- inside of ParsecT parser.
-
 module Day12 (day12) where
+
+import Data.Char (isAsciiLower, ord)
+import Data.List (findIndex, findIndices)
+import Data.Vector.Generic (streamR)
+
+parseString :: String -> ([Maybe Int], [Maybe Int])
+parseString input_text =
+    let
+        start_idx = map checkStart $ lines input_text
+        end_idx = map checkStart $ lines input_text
+     in
+        (start_idx, end_idx)
+
+checkStart :: [Char] -> Maybe Int
+checkStart = findIndex $ \s -> s == 'S'
+
+checkEnd :: [Char] -> Maybe Int
+checkEnd = findIndex $ \s -> s == 'E'
+
+findStartEnd :: [String] -> Maybe Int -> Maybe Int -> Maybe (Int, Int)
+findStartEnd [] Nothing _ = Nothing
+findStartEnd [] _ Nothing = Nothing
+findStartEnd text (Just startIdx) (Just endIdx) = Just (startIdx, endIdx)
+findStartEnd (row : nextRows) Nothing Nothing =
+    let
+        startIdx = checkStart row
+        endIdx = checkEnd row
+     in
+        findStartEnd nextRows startIdx endIdx
+findStartEnd (row : nextRows) (Just startIdx) Nothing =
+    let
+        endIdx = checkEnd row
+     in
+        findStartEnd nextRows (Just startIdx) endIdx
+findStartEnd (row : nextRows) Nothing (Just endIdx) =
+    let
+        startIdx = checkStart row
+     in
+        findStartEnd nextRows startIdx (Just endIdx)
+
+letter2elevation :: Char -> Int
+letter2elevation 'S' = ord 'a' - ord 'a'
+letter2elevation 'E' = ord 'z' - ord 'a'
+letter2elevation c = ord c - ord 'a'
 
 day12 :: IO ()
 day12 = do
+    txt <- readFile "task_12.txt"
     print "yay!"

8364f0e2a1f64b0b5498def9da3d38312c03e5ca
Manshilin Oleg <manshilin.o@gmail.com>
2025-10-06 01:57:29 +0300

    Added ability to build docker imahe using nix

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        exeName = "adventofcode2022";
 
         # Helper: build a dev shell for a given haskell package set (i.e., a specific GHC)
         mkShellFor =
@@ -65,17 +66,71 @@
         # Helper to make a nix run app for a given drv+exe
         mkAppFor = drv: {
           type = "app";
-          program = "${drv}/bin/adventofcode2022";
+          program = "${drv}/bin/${exeName}";
+        };
+
+        mkImage =
+          { name, drv }:
+          pkgs.dockerTools.buildLayeredImage {
+            inherit name;
+            tag = "latest";
+            contents = [
+              drv
+              pkgs.cacert
+              pkgs.bashInteractive # useful for debugging containers
+              pkgs.coreutils # minimal tools
+            ];
+            config = {
+              # Run the app by default; change or add args if needed
+              Cmd = [ "${drv}/bin/${exeName}" ];
+              # Basic environment (locale optional; uncomment if you need it)
+              # Env = [
+              #   "LANG=C.UTF-8"
+              #   "LC_ALL=C.UTF-8"
+              #   "LOCALE_ARCHIVE=${pkgs.glibcLocales}/lib/locale/locale-archive"
+              # ];
+              WorkingDir = "/work";
+            };
+            # Optional: add a writable /work
+            extraCommands = ''
+              mkdir -p work
+            '';
+          };
+
+        # Build derivations for each compiler
+        project94 = mkProject ghc94;
+        project96 = mkProject ghc96;
+        project98 = mkProject ghc98;
+
+        image94 = mkImage {
+          name = "${exeName}-ghc94";
+          drv = project94;
+        };
+        image96 = mkImage {
+          name = "${exeName}-ghc96";
+          drv = project96;
+        };
+        image98 = mkImage {
+          name = "${exeName}-ghc98";
+          drv = project98;
         };
       in
       {
         packages = {
-          project-ghc94 = mkProject ghc94;
-          project-ghc96 = mkProject ghc96;
-          project-ghc98 = mkProject ghc98;
+          project-ghc94 = project94;
+          project-ghc96 = project96;
+          project-ghc98 = project98;
 
-          # Default build target (change if you prefer a different default GHC)
-          default = mkProject ghc96;
+          # Default build target
+          default = project96;
+
+          # Docker/OCI images
+          dockerImage-ghc94 = image94;
+          dockerImage-ghc96 = image96;
+          dockerImage-ghc98 = image98;
+
+          # Default docker build target
+          dockerImage = image96;
         };
 
         # Build checks (handy for CI and `nix flake check`)

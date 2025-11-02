6a0bd277dfe1021e01d737875c9256860c265ea1
Oleg Manshilin <manshilin.o@gmail.com>
2025-11-02 19:41:54 +0000

    Prepare parser for part 2 of day 12.

--- src/Day12.hs
+++ src/Day12.hs
@@ -34,11 +34,11 @@ getMap (Maze map _ _) = map
 getMaxCoord :: Maze -> (Int, Int)
 getMaxCoord (Maze _ maxRow maxColumn) = (maxRow, maxColumn)
 
-parse :: String -> (Maybe (MazeCoord, MazeCoord), Maze)
+parse :: String -> (Maybe (MazeCoord, [MazeCoord], MazeCoord), Maze)
 parse inputText =
     let
         inputMatrixStr = lines inputText
-        startEndIndexes = findStartEnd inputMatrixStr 0 Nothing Nothing
+        startEndIndexes = findStartEnd inputMatrixStr 0 Nothing [] Nothing
         numRows = length inputMatrixStr
         numColumns = length $ head inputMatrixStr
     in
@@ -68,23 +68,34 @@ findStartEnd ::
     [String] ->
     Int ->
     Maybe MazeCoord ->
+    [MazeCoord] ->
     Maybe MazeCoord ->
-    Maybe (MazeCoord, MazeCoord)
-findStartEnd [] rowIdx Nothing _ = error "Should be impossible in this task"
-findStartEnd [] rowIdx _ Nothing = error "Should be impossible in this task"
-findStartEnd text rowIdx (Just startIdx) (Just endIdx) = Just (startIdx, endIdx)
-findStartEnd (row : nextRows) rowIdx Nothing Nothing =
+    Maybe (MazeCoord, [MazeCoord], MazeCoord)
+findStartEnd [] _ (Just startIdx) possibleStarts (Just endIdx) = Just (startIdx, startIdx:possibleStarts, endIdx)
+findStartEnd [] _ Nothing _ _ = error "Should be impossible in this task"
+findStartEnd [] _ _ _ Nothing = error "Should be impossible in this task"
+findStartEnd (row : nextRows) rowIdx Nothing possibleStarts Nothing =
     let
         startIdx = fromList <$> sequence [Just rowIdx, checkStart row]
         endIdx = fromList <$> sequence [Just rowIdx, checkEnd row]
+        possibleStartsAppend = foldl' (\x xs -> MazeCoord rowIdx xs : x) possibleStarts (findIndices (== 'a') row)
     in
-        findStartEnd nextRows (rowIdx + 1) startIdx endIdx
-findStartEnd (row : nextRows) rowIdx (Just startIdx) Nothing =
-    let endIdx = fromList <$> sequence [Just rowIdx, checkEnd row]
-    in  findStartEnd nextRows (rowIdx + 1) (Just startIdx) endIdx
-findStartEnd (row : nextRows) rowIdx Nothing (Just endIdx) =
-    let startIdx = fromList <$> sequence [Just rowIdx, checkStart row]
-    in  findStartEnd nextRows (rowIdx + 1) startIdx (Just endIdx)
+        findStartEnd nextRows (rowIdx + 1) startIdx possibleStartsAppend endIdx
+findStartEnd (row : nextRows) rowIdx (Just startIdx) possibleStarts Nothing =
+    let
+        endIdx = fromList <$> sequence [Just rowIdx, checkEnd row]
+        possibleStartsAppend = foldl' (\x xs -> MazeCoord rowIdx xs : x) possibleStarts (findIndices (== 'a') row)
+    in
+        findStartEnd nextRows (rowIdx + 1) (Just startIdx) possibleStartsAppend endIdx
+findStartEnd (row : nextRows) rowIdx Nothing possibleStarts (Just endIdx) =
+    let
+        startIdx = fromList <$> sequence [Just rowIdx, checkStart row]
+        possibleStartsAppend = foldl' (\x xs -> MazeCoord rowIdx xs : x) possibleStarts (findIndices (== 'a') row)
+    in
+        findStartEnd nextRows (rowIdx + 1) startIdx possibleStartsAppend (Just endIdx)
+findStartEnd (row : nextRows) rowIdx startIdx possibleStarts endIdx =
+    let possibleStartsAppend = foldl' (\x xs -> MazeCoord rowIdx xs : x) possibleStarts (findIndices (== 'a') row)
+    in  findStartEnd nextRows (rowIdx + 1) startIdx possibleStartsAppend endIdx
 
 letter2elevation :: Char -> Int
 letter2elevation 'S' = ord 'a' - ord 'a'
@@ -153,7 +164,7 @@ aStar startNode endNode maze@(Maze _ maxRows maxColumns) =
         initialMazeBool = V.replicate maxRows (V.replicate maxColumns False)
         -- Start with f-score = heuristic distance
         initialFScore = manhattanDistance startNode endNode
-        initialGscore =  setGScore (V.replicate maxRows (V.replicate maxColumns Nothing)) startNode 0
+        initialGscore = setGScore (V.replicate maxRows (V.replicate maxColumns Nothing)) startNode 0
     in
         aStar'
             (PSQ.singleton startNode initialFScore)
@@ -173,7 +184,7 @@ aStar startNode endNode maze@(Maze _ maxRows maxColumns) =
         | PSQ.null pqNodes = Nothing
         | currNode == endNode = Just currG
         -- probably need to use decrease key for repeated nodes. But I don't know how to do it in haskell right now
-        -- | visitedCheck mazeBool currNode = aStar' pqNodesRest mazeBool gScores
+        -- \| visitedCheck mazeBool currNode = aStar' pqNodesRest mazeBool gScores
         | otherwise =
             let
                 -- Mark current node as visited
@@ -182,11 +193,7 @@ aStar startNode endNode maze@(Maze _ maxRows maxColumns) =
                 validNeighbors = neighborsClimbOK maze newMazeBool currNode
                 -- Calculate scores for each neighbor
                 neighborScores =
-                    [ (gScore, hScore, node, isEmpty) | node <- validNeighbors,
-                        let gScore = currG + 1,
-                        let hScore = manhattanDistance node endNode,
-                        let isEmpty = isNothing (gScoreAt gScores node),
-                        isNothing (gScoreAt gScores node) || gScore < fromJust (gScoreAt gScores node)
+                    [ (gScore, hScore, node, isEmpty) | node <- validNeighbors, let gScore = currG + 1, let hScore = manhattanDistance node endNode, let isEmpty = isNothing (gScoreAt gScores node), isNothing (gScoreAt gScores node) || gScore < fromJust (gScoreAt gScores node)
                     ]
                 -- Get valid neighbors
                 newGScores = foldl' (\inGscore (g, _, n, _) -> setGScore inGscore n g) gScores neighborScores
@@ -194,8 +201,9 @@ aStar startNode endNode maze@(Maze _ maxRows maxColumns) =
                 newPQ =
                     foldl'
                         ( \pq (g, h, n, isEmpty) ->
-                            if isEmpty then PSQ.insert n (g + h) pq
-                            else PSQ.update (\p -> Just $ g + h) n pq
+                            if isEmpty
+                                then PSQ.insert n (g + h) pq
+                                else PSQ.update (\p -> Just $ g + h) n pq
                         )
                         pqNodesRest
                         neighborScores
@@ -208,12 +216,13 @@ aStar startNode endNode maze@(Maze _ maxRows maxColumns) =
 
 day12 :: IO ()
 day12 = do
-    txt <- readFile "task_12.txt"
+    txt <- readFile "task12_test.txt"
     (indexes, maze) <- return . parse $ txt
     indexes <- return . fromJust $ indexes
-    let (start, end) = indexes
+    let (start, possibleStarts, end) = indexes
     print $ "Start: " ++ show start
     print $ "End: " ++ show end
+    print $ "Part 2 starts: " ++ show possibleStarts
 
     case aStar start end maze of
         Nothing -> putStrLn "No path found"
